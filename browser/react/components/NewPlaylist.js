6a1b3142bbcd1942527f0cff6aa6534878257764
kyusulamita <sulamitamorales@yahoo.com>
2017-07-06 18:10:19 -0400

    calls

--- browser/react/components/NewPlaylist.js
+++ browser/react/components/NewPlaylist.js
@@ -5,7 +5,8 @@ export default class NewPlaylist extends React.Component {
   constructor () {
     super()
     this.state = {
-      value: ''
+      value: '',
+      disabled : false
     }
     this.handleOnSubmit = this.handleOnSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
@@ -13,11 +14,13 @@ export default class NewPlaylist extends React.Component {
   handleOnSubmit(event){
     event.preventDefault()
     console.log(this.state.value)
+    this.setState({value: ''})
   }
 
   handleChange(event) {
     this.setState({value:event.target.value})
-    //console.log(event.target.value)
+    if(event.target.value.length > 16 || !event.target.value.length) this.setState({disabled:true})
+    else this.setState({disabled: false})
   }
 
   render () {
@@ -28,16 +31,19 @@ export default class NewPlaylist extends React.Component {
           <div className="form-group">
             <label className="col-xs-2 control-label">Name</label>
             <div className="col-xs-10">
-              <input className="form-control" type="text" onChange={this.handleChange}/>
+              <input className="form-control" type="text" onChange={this.handleChange} value={this.state.value} required />
             </div>
           </div>
           <div className="form-group">
             <div className="col-xs-10 col-xs-offset-2">
-              <button type="submit" className="btn btn-success">Create Playlist</button>
+              <button type="submit" className="btn btn-success" disabled={this.state.disabled} >Create Playlist</button>
             </div>
           </div>
         </fieldset>
       </form>
+      {
+        this.state.disabled ? <div className="alert alert-warning">This is an invalid input</div> : <div></div>
+      }
     </div>
   }
 
